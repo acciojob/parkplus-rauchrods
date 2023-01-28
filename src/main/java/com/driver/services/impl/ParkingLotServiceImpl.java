28fbc48c9fbbd481bb57bdd0dd2ab8b470113553
rauch <rauchrodrigues@gmail.com>
2023-01-28 16:54:58 +0530

    2nd commit

--- src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
@@ -29,10 +29,10 @@ public class ParkingLotServiceImpl implements ParkingLotService {
     @Override
     public Spot addSpot(int parkingLotId, Integer numberOfWheels, Integer pricePerHour) {
         SpotType spotType;
-        if (numberOfWheels==2){
+        if (numberOfWheels<=2){
             spotType=SpotType.TWO_WHEELER;
         }
-        else if (numberOfWheels==4) {
+        else if (numberOfWheels>2 && numberOfWheels<=4) {
             spotType=SpotType.FOUR_WHEELER;
         }
         else {
